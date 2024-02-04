c29718a0c121e2ac8297c2d5eef5a21584e6cfcb
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-04 23:15:19 +0400

    Add files via upload

---
+++ lesson1-hello-world/lesson1-hello-world/Program.cs
@@ -0,0 +1,18 @@
+﻿//Console.WriteLine("Hello, World!");
+//say HelloWorld!
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("c# programming");
+
+            Console.Write("Gulnara");
+            Console.Write("Garibova");
+        }
+    }
+}
+//WriteLine ilə Write ekrana çıxartmaq üçün istifadə edilir, fərq budur ki ilki ilə yazdığımız bir neçə sətir alt-alta yazıldığı halda, ikincisində yazdığımız sətirlər isə boşluq olmadan birləşik halda yazılır.
