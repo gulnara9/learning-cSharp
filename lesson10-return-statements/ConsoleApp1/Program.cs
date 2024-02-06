fc9d15a48c3de7cf5ec1ae91905dc535b03a2987
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-06 23:30:08 +0400

    Add files via upload

---
+++ lesson10-return-statements/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube(5));//5 dəyəri verərək cube funksiyasını çalışdırırıq
+            Console.ReadLine();
+        }
+        static int cube(int num)//Bu funksiya, bir int parametri alır ve bu parametrin kubunu hesaplayır.
+        {
+            int result = num * num * num;
+            return result;//Hesaplanan nəticə result dəyişənində saxlanır ve return result ifadesi ile geri döndürülür.
+        }
+    }
+}
