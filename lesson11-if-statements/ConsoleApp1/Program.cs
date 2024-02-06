1221cddf4a5daad32a85bbb11b87c19be570e3b4
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-07 00:57:54 +0400

    Add files via upload

---
+++ lesson11-if-statements/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale= false;
+            if (isMale)
+            {
+                Console.WriteLine("You are male");//əgər isMale true olarsa o zaman bu kod bloku icra olunacaq, əks halda else bloku icra olunacaq
+            }
+            else {
+                Console.WriteLine("You are not male");
+            }
+             isMale = true;
+
+            bool isTall = true;
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male ");//
+            }
+            else
+            {
+                Console.WriteLine("You are either not male or tall or both");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ lesson12-building-calculator2/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a Operator: ");
+            string op = Console.ReadLine();
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            if (op == "+")
+            {
+                Console.WriteLine(num2 + num1);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num2 - num1);
+            }
+            else if(op == "*")
+            {
+                Console.WriteLine(num2 * num1);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num2 / num1);
+            }
+            else
+            {
+                Console.WriteLine("invalid operator");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
