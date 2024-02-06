ddf91a841709f321010efe5ef3979ed818fc4ac2
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-06 22:51:12 +0400

    Add files via upload

---
+++ lesson8-arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+            myArray[0] = 10;//y'ni array-in il elementi 10-dur
+            int [] newArray = { 12, 45, 64, 3, 6, 1, 97, 53 };
+            //array-in elementlərini ekrana yazdırmmaq üçün 2 üsuldan istifadə edə bilərik:
+            //foreach metodu ilə
+            foreach (int i in newArray)
+            {
+                Console.WriteLine(i);//alt-alta ekrana çıxaracaq
+            }
+            //string.Join metodu ilə
+            Console.WriteLine(string.Join(", ", myArray));//yan-yana, vergüllə ayrılmış şəkildə ekrana çıxaracaq
+            //////////////////
+            //array-in hər hansısa elementini update edə bilərik
+            newArray[6] = 56;//array-dakı 6-cı elementi yəni 97-ni 56 ilə əvəzləyəcək
+            Console.WriteLine(newArray[6]);
+
+            /////string array
+            string[] programmingLangs = {"c#","python","java","c++" };
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson9-methods/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHi("gulnara",21);//sayHi funksiyasını çağırıb, ona "gulnara" və 21 dəyərini ötürürük
+            Console.ReadLine();
+
+        }
+        static void sayHi(string name, int age)// sayHi funksiyası bir string dəyər, bir integer dəyər alacağını söyləyirik
+        {
+            Console.WriteLine($"hello,{name}, you're {age}");
+        }
+    }
+}
