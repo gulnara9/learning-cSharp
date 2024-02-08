41a0ed99c271c3e734d024749c05f4ab80c0bb62
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-08 15:56:12 +0400

    Add files via upload

---
+++ lesson17-2d-arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] gridNums = //[,] ikiölçülü array yaratmaq 
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            };
+            Console.WriteLine(gridNums[2, 1]);// bu o deməkdir ki, gridNums daxilindəki indeksi 2 olan arrayin indeksi 1 olan elementini  gətir
+
+            int[,] newArray = new int[2, 3];// əgər arrayda hansı elementləri yerləşdirmək istədiyimizi bilmiriksə, o zaman belə yaza bilərik, bu o anlama gəlir ki, neçə sətir neçə stüna sahib olmaq istəyirik. yəni array daxilində 2 element var və hər biri 3 element daşıyır daxilində. və sonra elementləri əl ilə daxil edə bilərik.
+            Console.ReadLine();
+        }
+    }
+}
