da73b5ba0c9645aa4a366e8c984dce5ef884322e
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-19 22:51:24 +0400

    Add files via upload

---
+++ lesson21-getters-setters/ConsoleApp1/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+
+        }
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "a" || value == "b" || value == "c" || value=="d")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "d";
+                }
+
+            }
+        }
+
+    }
+}
---
+++ lesson21-getters-setters/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie harrypotter = new Movie("aaaaa", "bbbbb", "a");
+            Movie askimemnun = new Movie("cccc", "dddd", "b");
+
+            harrypotter.Rating = "v";
+            Console.WriteLine(harrypotter.Rating);
+            Console.ReadLine();
+        }
+    }
+}
