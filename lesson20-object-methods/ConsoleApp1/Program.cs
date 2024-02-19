4f1c90f6fc342a84898073a809a724114e690f66
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-19 22:31:01 +0400

    Add files via upload

---
+++ lesson20-object-methods/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student1 = new Students("Gulnara", "Web Development", 3.6);
+            Students student2= new Students("Numi", "Design", 2.9);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson20-object-methods/ConsoleApp1/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Students
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Students(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
