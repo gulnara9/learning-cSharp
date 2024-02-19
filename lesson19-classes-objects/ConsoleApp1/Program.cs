f5dfb8c1f08ca3e059364b2feeccb2f1cefde32f
gulnara <123542083+gulnara9@users.noreply.github.com>
2024-02-19 17:28:17 +0400

    Add files via upload

---
+++ lesson19-classes-objects/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1=new Book("HP","Rowling",450);
+            //book1.title = "My sweet Orange Tree";
+            //book1.author = "José Mauro de Vasconcelos";
+            //book1.pages = 345;
+            Book book2 = new Book("LotR", "Tolkien", 650);
+            Book book3 = new Book("My sweet Orange Tree", "José Mauro de Vasconcelos", 400);
+
+            Console.WriteLine(book1);
+            Console.ReadLine(); 
+        }
+    }
+}
